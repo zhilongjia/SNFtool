5ed318d34c77ec95b471d2ab0e8e452b6f0114d7
Zhilong Jia <zhilongjia@gmail.com>
2021-02-18 12:13:53 +0800

    add parallel dist in groupPredict.r.

--- R/groupPredict.r
+++ R/groupPredict.r
@@ -1,4 +1,4 @@
-groupPredict <- function(train, test, groups, K=20, alpha=0.5, t=20, method=1){
+groupPredict <- function(train, test, groups, K=20, alpha=0.5, t=20, method=1, verbose=TRUE){
     # Predicts subtype of new patients from labeled training set of patients 
     #   using label propigation or local and global consistency.
     #
@@ -15,25 +15,31 @@ groupPredict <- function(train, test, groups, K=20, alpha=0.5, t=20, method=1){
     #
     # Returns: 
     #   Vector of new labels assigned to the test samples 
+    
+    # update dist method
     Wi <- vector("list", length=length(train))
     
     for (i in 1:length(train)){
-        view <- standardNormalization(rbind(train[[i]],test[[i]]))
-        Dist1 <- dist2(view, view)
-        Wi[[i]] <- affinityMatrix(Dist1, K, alpha)
+        view_i <- standardNormalization(rbind(train[[i]],test[[i]]))
+        # Dist1 <- dist2(view_i, view_i)
+        if (verbose) {print (paste("Start Dist", i))}
+        Dist2 <- amap::Dist(view_i, nbproc=15)
+        Wi[[i]] <- affinityMatrix(as.matrix(Dist2), K, alpha)
     }
     
     W <- SNF(Wi,K,t)
-    Y0 <- matrix(0,nrow(view), max(groups))
+    Y0 <- matrix(0,nrow(view_i), max(groups))
     for (i in 1:length(groups)){
         Y0[i,groups[i]] <- 1
     }
-
+    
+    if (verbose) {print ("Start .csPrediction")}
     Y <- .csPrediction(W,Y0,method)
-    newgroups <- rep(0,nrow(view))
+    newgroups <- rep(0,nrow(view_i))
     for (i in 1:nrow(Y)){
         newgroups[i] <- which(Y[i,] == max(Y[i,]))
     }
     
-    return (newgroups)
+    
+    return (list(W=W, Y=Y, newgroups=newgroups))
 }
